09ac77aa28b67b8e6200f2ce325630010538cafa
hahoetal <yeong-eun@likelion.org>
2020-10-22 04:03:09 +0900

    01 ~ 03장 2020.10.22

---
+++ 01~03.R
@@ -0,0 +1,17 @@
+# Q1. 시험 점수 변수 만들고 출력하기
+score <- c(80, 60, 70, 50, 90)
+score
+
+# Q2. 전체 평균 구하기
+mean(score)
+
+# Q3. 전체 평균 변수 만들고 출력하기
+score_mean <- mean(score)
+score_mean
+
+# Argument와 Parameter
+# Argument: 인수, 매개 변수에 담기는 실제 값.
+# Parameter: 매개 변수, 함수를 정의할 때 사용하는 변수.
+
+# 패키지 설치하기: install.packages("설치하고 싶은 패키지 이름")
+# 패키지 가져오기: library(가져올 패키지 이름)
